74749f1871bdb658b0781ba809dd8695195fd199
Afifizzudn <surel.afif@gmail.com>
2019-09-09 09:45:59 +0700

    update login phone number

--- app/src/main/java/com/google/firebase/afifizzudn/InternalChat/SignInActivity.java
+++ app/src/main/java/com/google/firebase/afifizzudn/InternalChat/SignInActivity.java
@@ -19,8 +19,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -32,10 +36,17 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignInActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
@@ -44,8 +55,20 @@ public class SignInActivity extends AppCompatActivity implements
     private static final int RC_SIGN_IN = 9001;
 
     private SignInButton mSignInButton;
-
     private GoogleApiClient mGoogleApiClient;
+
+    //Variable Untuk Komponen-komponen Yang Diperlukan
+    private EditText NoTelepon, SetKode;
+    private Button Masuk, Verifikasi, Resend;
+    private TextView PhoneID;
+
+    //Variable yang Dibutuhkan Untuk Autentikasi
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener stateListener;
+    private PhoneAuthProvider.ForceResendingToken resendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
+    private String VerifikasiID;
+    private String No_Telepon;
 
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
@@ -57,9 +80,20 @@ public class SignInActivity extends AppCompatActivity implements
 
         // Assign fields
         mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        NoTelepon = (EditText) findViewById(R.id.phone);
+        SetKode = (EditText) findViewById(R.id.setVertifi);
+        Masuk = (Button) findViewById(R.id.login);
+        Verifikasi = (Button) findViewById(R.id.verifi);
+        Resend = (Button) findViewById(R.id.resend);
+        PhoneID = (TextView) findViewById(R.id.no_id);
 
         // Set click listeners
         mSignInButton.setOnClickListener(this);
+        Masuk.setOnClickListener(this);
+        Verifikasi.setOnClickListener(this);
+        Resend.setOnClickListener(this);
+        Resend.setEnabled(false);
+
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -73,6 +107,18 @@ public class SignInActivity extends AppCompatActivity implements
 
         // Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
+        stateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //Meneteksi Apakah Ada User Yang Sedang Login (Belum Logout)
+                if(user != null){
+                    //Jika Ada, User Tidak perlu Login Lagi, dan Langsung Menuju Acivity Yang Dituju
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
@@ -81,6 +127,44 @@ public class SignInActivity extends AppCompatActivity implements
             case R.id.sign_in_button:
                 signIn();
                 break;
+            case R.id.login:
+                No_Telepon = PhoneID.getText()+NoTelepon.getText().toString();
+                setupVerificationCallback();
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        No_Telepon, //NO Telepon Untuk Di Verifikasi
+                        60, //Durasi Waktu Habis
+                        TimeUnit.SECONDS, //Unit Timeout
+                        this, //Activity
+                        callbacks); // OnVerificationStateChangedCallbacks
+                Toast.makeText(getApplicationContext(), "Memverifikasi, Mohon Tunggu", Toast.LENGTH_SHORT).show();
+                NoTelepon.setText("");
+                break;
+
+            case R.id.verifi:
+                String verifiCode = SetKode.getText().toString();
+                if(TextUtils.isEmpty(verifiCode)){
+                    //Memberi Pesan pada user bahwa kode verifikasi tidak boleh kosong saat menekan Tombol Verifikasi
+                    Toast.makeText(getApplicationContext(),"Masukan Kode Verifikasi", Toast.LENGTH_SHORT).show();
+                }else{
+                    //Memverifikasi Nomor Telepon, Saat Tombol Verifikasi Ditekan
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(VerifikasiID, verifiCode);
+                    signInWithPhoneAuthCredential(credential);
+                    Toast.makeText(getApplicationContext(),"Sedang Memverifikasi", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.resend:
+                No_Telepon = PhoneID.getText()+NoTelepon.getText().toString();
+                setupVerificationCallback();
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        No_Telepon, //NO Telepon Untuk Di Vertifikai
+                        60, //Durasi Waktu Habis
+                        TimeUnit.SECONDS, //Unit Timeout
+                        this, //Activity
+                        callbacks, // OnVerificationStateChangedCallbacks
+                        resendToken); // Digunakan untuk mengirim ulang kembali kode verifikasi
+                Toast.makeText(getApplicationContext(), "Mengirim Ulang Kode Verifikasi", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 
@@ -88,6 +172,72 @@ public class SignInActivity extends AppCompatActivity implements
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Melampirkan Listener pada FirebaseAuth saat Activity Dimulai
+        mFirebaseAuth.addAuthStateListener(stateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(stateListener != null){
+            //Menghapus Listener pada FirebaseAuth saat Activity Dihentikan
+            mFirebaseAuth.removeAuthStateListener(stateListener);
+        }
+    }
+
+    private void setupVerificationCallback(){
+        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken token) {
+                // Callback didalam sini akan dipanggil/dieksekusi saat terjadi proses pengiriman kode
+                // Dan User Diminta untuk memasukan kode verifikasi
+
+                // Untuk Menyimpan ID verifikasi dan kirim ulang token
+                VerifikasiID = verificationId;
+                resendToken = token;
+                Resend.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Mendapatkan Kode Verifikasi", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential Credential) {
+                // Callback disini akan dipanggil saat Verifikasi Selseai atau Berhasil
+                Toast.makeText(getApplicationContext(), "Verifikasi Selesai", Toast.LENGTH_SHORT).show();
+                signInWithPhoneAuthCredential(Credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // Callback disini akan dipanggil saat permintaan tidak valid atau terdapat kesalahan
+                Toast.makeText(getApplicationContext(), "Verifikasi Gagal, Silakan Coba Lagi", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential){
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Sign In Berhasil
+                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                            finish();
+                        }else{
+                            //Sign In Gagal
+                            if(task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+                                // Kode Yang Dimasukan tidal Valid.
+                                Toast.makeText(getApplicationContext(), "Kode yang dimasukkan tidak valid", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
